45089724789a22f29ac146a0e2c509f879c9578f
Beatrice Kwan <beatrice.c.kwan@gmail.com>
2021-07-14 21:17:08 +0000

    add function to load past tweets to page using ajax get request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,33 +6,34 @@
 
 
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 
 $(document).ready(function() {
+  
   //For each tweet object, render and append tweet element
   const renderTweets = function(tweets) {
     tweets.forEach(function(tweet) {
@@ -68,7 +69,7 @@ $(document).ready(function() {
     return $tweet;
   }
   
-  renderTweets(data);
+
 
 
   //Event handler for new tweets
@@ -78,7 +79,18 @@ $(document).ready(function() {
     $.post('/tweets', serializedData);
   })
 
+
+  //Function to request and load array of tweets
+  const loadTweets = function() {
+    $.ajax('/tweets', {method: 'GET'})
+    .then(function(data) {
+      renderTweets(data);
+    });
+  }
+
+  loadTweets();
   
+
 })
 
 
